71a635c51f765ca0d92a32bc208ebe5b745f8fc7
im-adithya <imadithyavardhan@gmail.com>
2023-05-02 13:36:32 +0530

    feat: add jupyter icons

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import { CollaborateIcon, OrganisationIcon, SchoolIcon, StudentIcon } from "../react/outline";
-import { AcademicCapIcon, DaskLogoIcon, DatalayerLogoIcon, DatalayerLogoBlackIcon, DatalayerLogoColorIcon, DatalayerLogoGreenIcon, DatalayerLogoWhiteIcon, KubernetesLogoIcon, KubernetesNodeIcon, KubernetesPodIcon, PyTorchLogoIcon, ReactLogoIcon, TensorFlowLogoIcon, TwitterLogoIcon, WebRtcLogoIcon } from "../react/solid";
+import { AcademicCapIcon, DaskLogoIcon, DatalayerLogoIcon, DatalayerLogoBlackIcon, DatalayerLogoColorIcon, DatalayerLogoGreenIcon, DatalayerLogoWhiteIcon,JupyterHubIcon, JupyterHubServiceLogoIcon, JupyterKernelGatewayLogoIcon, JupyterKernelLogoIcon, JupyterKernelProvisionerLogoIcon, JupyterLabLogoIcon, JupyterLogoIcon, JupyterMystLogoIcon, JupyterServerLogoIcon, KubernetesLogoIcon, KubernetesNodeIcon, KubernetesPodIcon, PyTorchLogoIcon, ReactLogoIcon, TensorFlowLogoIcon, TwitterLogoIcon, WebRtcLogoIcon } from "../react/solid";
 
 const icons = {
   AcademicCap: AcademicCapIcon,
   Collaborate: CollaborateIcon,
   Dask: DaskLogoIcon,
-  DatalayerLogo: DatalayerLogoIcon,
-  DatalayerLogoBlack: DatalayerLogoBlackIcon,
-  DatalayerLogoColor: DatalayerLogoColorIcon,
-  DatalayerLogoGreen: DatalayerLogoGreenIcon,
-  DatalayerLogoWhite: DatalayerLogoWhiteIcon,
-  KubernetesLogo: KubernetesLogoIcon,
+  Datalayer: DatalayerLogoIcon,
+  DatalayerBlack: DatalayerLogoBlackIcon,
+  DatalayerColor: DatalayerLogoColorIcon,
+  DatalayerGreen: DatalayerLogoGreenIcon,
+  DatalayerWhite: DatalayerLogoWhiteIcon,
+  Jupyter: JupyterLogoIcon,
+  JupyterServer: JupyterServerLogoIcon,
+  JupyterLab: JupyterLabLogoIcon,
+  JupyterHub: JupyterHubIcon,
+  JupyterHubService: JupyterHubServiceLogoIcon,
+  JupyterKernel: JupyterKernelLogoIcon,
+  JupyterKernelGateway: JupyterKernelGatewayLogoIcon,
+  JupyterKernelProvisioner: JupyterKernelProvisionerLogoIcon,
+  JupyterMyst: JupyterMystLogoIcon,
+  Kubernetes: KubernetesLogoIcon,
   KubernetesNode: KubernetesNodeIcon,
   KubernetesPod: KubernetesPodIcon,
   Organisation: OrganisationIcon,
